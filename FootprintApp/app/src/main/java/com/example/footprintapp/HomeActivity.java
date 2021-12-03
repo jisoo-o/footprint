7f8df886a9e83d729beb5d03cef3be4bd12f93c9
Jisoo Park <cosmopark624@gmail.com>
2021-12-04 01:30:55 +0900

    feat : 권장 산책시간 계산 함수 구현, progress bar 생성

--- FootprintApp/app/src/main/java/com/example/footprintapp/HomeActivity.java
+++ FootprintApp/app/src/main/java/com/example/footprintapp/HomeActivity.java
@@ -15,13 +15,21 @@ public class HomeActivity extends AppCompatActivity implements CircleProgressBar
 
     private static final String DEFAULT_PATTERN = "%d%%";
     CircleProgressBar circleProgressBar;
+    CircleProgressBar circleProgressBar2;
 
     private ImageView imageview2;
     private TextView petname;
+
     private int eatProgess = 0;
-    private int taken = 80;
+    private int taken = 180;
+
+    private int playProgess = 0;
+    private int played = 10;
+
     private TextView recommendedCalories;
+    private TextView recommendedPlay;
     private int maintenanceER;
+    private int recommendPT;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -31,6 +39,7 @@ public class HomeActivity extends AppCompatActivity implements CircleProgressBar
         petname = findViewById(R.id.petName);
         imageview2 = findViewById(R.id.petImg);
         recommendedCalories = findViewById(R.id.recommend_calories);
+        recommendedPlay = findViewById(R.id.recommend_play);
 
         Intent reIndent = getIntent();
         String text = reIndent.getStringExtra("petName");
@@ -43,13 +52,18 @@ public class HomeActivity extends AppCompatActivity implements CircleProgressBar
         String petWeight= reIndent.getStringExtra("petWeight");
         int petWeightInt = Integer.parseInt(petWeight);
         maintenanceER = calculateCalories(petWeightInt);
+        recommendPT = calculatePlaytime(petWeightInt);
 
         eatProgess = taken * 100 / maintenanceER;
         circleProgressBar=findViewById(R.id.eat_circlebar);
         circleProgressBar.setProgress(eatProgess);  // 해당 퍼센트를 적용
 
+        playProgess = played * 100 / recommendPT;
+        circleProgressBar2=findViewById(R.id.play_circlebar);
+        circleProgressBar2.setProgress(playProgess);  // 해당 퍼센트를 적용
 
         recommendedCalories.setText("권장 섭취량은 " + maintenanceER + "kcal이에요");
+        recommendedPlay.setText("최소 " + recommendPT + "분은 뛰어 놀고 싶어요");
     }
     @Override
     public CharSequence format(int progress, int max) {
@@ -66,6 +80,18 @@ public class HomeActivity extends AppCompatActivity implements CircleProgressBar
 
         maintenaceEnergyRequirement = (int) (restingEnergyRequirements * 1.6);
         return maintenaceEnergyRequirement;
+    }
+
+    private int calculatePlaytime(int weight) {
+        int recommendedPlayTime;
+        if(weight <7){
+            recommendedPlayTime = 25;
+        }else if(7<weight && weight<15){
+            recommendedPlayTime = 30;
+        }else{
+            recommendedPlayTime = 45;
+        }
+        return recommendedPlayTime;
     }
 
 
